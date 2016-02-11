0c32e6443de96dfd55198bd19149caf16f1bfd53
parry <parry@parrys-MacBook-Air.local>
2016-02-11 00:58:23 -0800

    cleaned

--- ReverseString.playground/Contents.swift
+++ ReverseString.playground/Contents.swift
@@ -7,18 +7,7 @@ func reverseString(var characters: [Character]) -> [Character]{
     
     func swapFunction(inout stringArray: [Character], currentchar: Int) {
         let swapIndex = stringArray.count - currentchar - 1
-        
         swap(&stringArray[currentchar], &stringArray[swapIndex])
-        
-    }
-    
-    func sliceFunction(inout stringArray: [Character], currentchar: Int, wordlength: Int, startindex: Int) {
-        
-        let swapIndex = (wordlength - currentchar - 1) + startindex
-        
-        
-        swap(&stringArray[currentchar], &stringArray[swapIndex])
-        
     }
     
     
@@ -29,15 +18,20 @@ func reverseString(var characters: [Character]) -> [Character]{
 }
 
 
-func sliceString(in out ){
+func sliceString(var characters: [Character]) -> [Character]{
+    
+    func sliceFunction(inout stringArray: [Character], currentchar: Int, wordlength: Int, startindex: Int) {
+        let swapIndex = (wordlength - currentchar - 1) + startindex
+        swap(&stringArray[currentchar], &stringArray[swapIndex])
+    }
+    
     
     var previousSpace = -1
-    for space in 0..<characters.count {
+    for var space = 0; space < characters.count; space++ {
         
         if characters[space] == " " || characters[space] == "\r\n" {
-            var middle = (space + previousSpace + 1)/2
-            var wordStartIndex = previousSpace + 1
-            var length = space - previousSpace - 1
+            let middle = (space + previousSpace + 1)/2
+            let wordStartIndex = previousSpace + 1
             
             for var charachter = previousSpace + 1; charachter < middle; ++charachter {
                 sliceFunction(&characters, currentchar: charachter, wordlength: space, startindex: wordStartIndex)
@@ -48,9 +42,8 @@ func sliceString(in out ){
         if space >= characters.count - 1  {
             space = characters.count
             
-            var middle = (space + previousSpace + 1)/2
-            var wordStartIndex = previousSpace + 1
-            var length = space - previousSpace - 1
+            let middle = (space + previousSpace + 1)/2
+            let wordStartIndex = previousSpace + 1
             
             for var charachter = previousSpace + 1; charachter < middle; ++charachter {
                 sliceFunction(&characters, currentchar: charachter, wordlength: space, startindex: wordStartIndex)
@@ -60,6 +53,7 @@ func sliceString(in out ){
         
         
     }
+    return characters
     
 }
 
@@ -67,7 +61,7 @@ let wordstr = "find you will pain only go you recordings security the into if"
 var characters = Array(wordstr.characters)
 
 let reversedString = reverseString(characters)
-let reverseSliceString = sliceString()
+let reverseSliceString = sliceString(reversedString)
 
 
 
